a78ba0e86232b52072b689b0b51a9d43fe155bc1
rknyman <rknyman@nymantechnology.com>
2018-05-20 14:05:10 -0700

    Update config.js
    changed the right variable

--- bin/config.js
+++ bin/config.js
@@ -14,7 +14,7 @@ var autoconfig  = function (config_overrides){
 
   var ws_info = {
     id: "nationalparks-rkn-js",
-    displayName: "National Parks (JS)",
+    displayName: "National Parks (rkn)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
