f9f29240518c605da3522b9d552f4f53031e626e
rknyman <rknyman@nymantechnology.com>
2018-05-20 14:11:21 -0700

    Update config.js
    fixed the other thing...on line 16

--- bin/config.js
+++ bin/config.js
@@ -13,7 +13,7 @@ var autoconfig  = function (config_overrides){
   })
 
   var ws_info = {
-    id: "nationalparks-rkn-js",
+    id: "nationalparks-js",
     displayName: "National Parks (rkn)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
