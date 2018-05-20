77d05b41586030f2b833c09a649b6bd82a0fdd22
rknyman <rknyman@nymantechnology.com>
2018-05-20 14:20:05 -0700

    Update config.js
    try, try again...

--- bin/config.js
+++ bin/config.js
@@ -14,7 +14,7 @@ var autoconfig  = function (config_overrides){
 
   var ws_info = {
     id: "nationalparks-js",
-    displayName: "RK's National Parks",
+    displayName: "RK's National Parks (JS)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
